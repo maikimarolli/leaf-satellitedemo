27ae8dc6fdf3e9711d77165fe4d68664ee5fcf11
Maiki Marolli <maiki.marolli@gmail.com>
2022-10-19 11:41:28 -0300

    validations

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -3,23 +3,44 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
+  private apiUrl = 'http://localhost:3000/';
 
-  private apiUrl = 'http://localhost:3000/'
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
+  login(email: any, password: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl + 'create_token', {
+      email: email,
+      password: password,
+    });
+  }
 
-  login (email: any, password: any) : Observable<any> {
-    return this.http.post<any>(this.apiUrl + 'create_token', {'email': email, 'password': password})
-  };
+  loadFields(): Observable<any> {
+    return this.http.get<any>(this.apiUrl + 'monitored_fields');
+  }
 
-  loadFields () : Observable<any> {
-    return this.http.get<any>(this.apiUrl + 'monitored_fields')
-  };
+  loadImages(fieldId: any, startDate: any, endDate: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl + 'field_images', {
+      id: fieldId,
+      startDate: startDate,
+      endDate: endDate,
+    });
+  }
 
-  loadImages (fieldId : any, startDate : any, endDate : any) : Observable<any> {
-    return this.http.post<any>(this.apiUrl + 'field_images', {'id': fieldId, 'startDate': startDate, 'endDate' : endDate})
+  createField(
+    fieldId: any,
+    daysBefore: any,
+    bounderyType: any,
+    fieldBoundery: any,
+    provider: any
+  ): Observable<any> {
+    return this.http.post<any>(this.apiUrl + 'create_field', {
+      field_name: fieldId,
+      boundery_type: bounderyType,
+      field_boundery: fieldBoundery,
+      days_before: daysBefore
+    });
   }
 }
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -3,7 +3,7 @@ import { NgxSidebarControlComponent } from '@runette/ngx-leaflet-sidebar';
 import * as L from 'leaflet';
 import { GeoJsonTypes } from 'geojson';
 import { AppService } from '../app.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AstMemoryEfficientTransformer } from '@angular/compiler';
 
 @Component({
@@ -16,6 +16,9 @@ export class MapComponent implements OnInit, OnDestroy {
   public isLoading: boolean = false;
   public isLogged: boolean = false;
   public loginMessage: string = '';
+  public createFieldMessage: string = '';
+  public loadImagesMessage: string = '';
+
   public fields: any;
   public images: any;
 
@@ -24,6 +27,7 @@ export class MapComponent implements OnInit, OnDestroy {
 
   loginForm!: FormGroup;
   searchImagesForm!: FormGroup;
+  createFieldForm!: FormGroup;
 
   private initMap(): void {
     this.map = L.map('map', {
@@ -84,17 +88,44 @@ export class MapComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.initMap();
     this.loginForm = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
+      email: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
     });
     this.searchImagesForm = new FormGroup({
-      fieldId: new FormControl(''),
-      startDate: new FormControl(''),
-      endDate: new FormControl(''),
+      fieldId: new FormControl('', [Validators.required]),
+      startDate: new FormControl('', [Validators.required]),
+      endDate: new FormControl('', [Validators.required]),
+    });
+    this.createFieldForm = new FormGroup({
+      fieldId: new FormControl('', [Validators.required]),
+      daysBefore: new FormControl('', [Validators.required]),
+      bounderyType: new FormControl('', [Validators.required]),
+      fieldBoundery: new FormControl('', [Validators.required]),
+      provider: new FormControl('', [Validators.required]),
     });
   }
 
   ngOnDestroy(): void {}
+
+  getCreateFieldId() {
+    return this.createFieldForm.get('fieldId')!;
+  }
+
+  getCreateDaysBefore() {
+    return this.createFieldForm.get('daysBefore')!;
+  }
+
+  getCreateBounderyType() {
+    return this.createFieldForm.get('bounderyType')!;
+  }
+
+  getCreateFieldBoundery() {
+    return this.createFieldForm.get('fieldBoundery')!;
+  }
+
+  getCreateProvider() {
+    return this.createFieldForm.get('provider')!;
+  }
 
   getEmail() {
     return this.loginForm.get('email')!;
@@ -130,22 +161,32 @@ export class MapComponent implements OnInit, OnDestroy {
 
   doLogin() {
     this.isLoading = true;
-    this.appService
-      .login(this.getEmail().value, this.getPassword().value)
-      .subscribe((data) => {
+    this.loginMessage = '';
+    let email = this.getEmail().value;
+    let password = this.getPassword().value;
+    if (email === '' || password === '') {
+      this.isLoading = false;
+      if (email === '') {
+        this.loginMessage = 'You need to insert your email first.';
+      } else {
+        this.loginMessage = 'You need to insert your password first.';
+      }
+    } else {
+      this.appService.login(email, password).subscribe((data) => {
         this.isLoading = false;
         this.loginMessage = data;
         if (data === 'Congrats! Login done!') {
           this.isLogged = true;
           document.getElementById('fields_a')!.click();
-          this.loadCreatedFeilds();
+          this.loadCreatedFields();
         } else {
           this.isLogged = false;
         }
       });
+    }
   }
 
-  loadCreatedFeilds() {
+  loadCreatedFields() {
     this.isLoading = true;
     this.appService.loadFields().subscribe((data) => {
       this.isLoading = false;
@@ -153,7 +194,7 @@ export class MapComponent implements OnInit, OnDestroy {
     });
   }
 
-  loadImages(fieldId: string) {
+  firstLoadImages(fieldId: string) {
     document.getElementById('field_images_a')!.click();
     this.isLoading = true;
     let endDate = new Date();
@@ -171,50 +212,55 @@ export class MapComponent implements OnInit, OnDestroy {
       .loadImages(fieldId, realStartDate, realEndDate)
       .subscribe((data) => {
         this.isLoading = false;
-        this.images = [];
-        this.images = data.images;
+        this.loadImagesMessage = '';
+        if (data === 'No images were found for the time and field selected') {
+          this.loadImagesMessage = data;
+        } else {
+          this.images = [];
+          this.images = data.images;
 
-        var geometry = data.geometry[0][0];
+          var geometry = data.geometry[0][0];
 
-        var json = {
-          type: 'Feature' as GeoJsonTypes,
-          geometry: {
-            type: 'Polygon',
-            coordinates: [geometry],
-          },
-        };
+          var json = {
+            type: 'Feature' as GeoJsonTypes,
+            geometry: {
+              type: 'Polygon',
+              coordinates: [geometry],
+            },
+          };
 
-        var polygonStyle = {
-          weight: 2,
-          opacity: 1,
-          fill: false,
-          'fill-opacity': 0,
-        } as L.GeoJSONOptions;
+          var polygonStyle = {
+            weight: 2,
+            opacity: 1,
+            fill: false,
+            'fill-opacity': 0,
+          } as L.GeoJSONOptions;
 
-        this.bounds.clearLayers();
-        this.imagens.clearLayers();
+          this.bounds.clearLayers();
+          this.imagens.clearLayers();
 
-        var geoJsonLayer = L.geoJson(json, polygonStyle).addTo(this.bounds);
-        var geoBounds = geoJsonLayer.getBounds();
-        this.map.flyTo(geoJsonLayer.getBounds().getCenter(), 15);
+          var geoJsonLayer = L.geoJson(json, polygonStyle).addTo(this.bounds);
+          var geoBounds = geoJsonLayer.getBounds();
+          this.map.flyTo(geoJsonLayer.getBounds().getCenter(), 15);
 
-        this.images.forEach((e: any) => {
-          e.images.forEach((f: any) => {
-            f.bounds = geoBounds;
-            f.date = e.date.substring(11, 23);
-            if (e.provider === 'sentinel') {
-              f.imgtype = f.url.split('/0/')[1].split('.')[0].split('_')[0];
-            } else if (e.provider === 'planet') {
-              f.imgtype = f.url
-                .split('_SR.tif/')[1]
-                .split('.')[0]
-                .split('_')[0];
-            } else {
-              f.imgtype = 'not supported';
-            }
+          this.images.forEach((e: any) => {
+            e.images.forEach((f: any) => {
+              f.bounds = geoBounds;
+              f.date = e.date.substring(11, 23);
+              if (e.provider === 'sentinel') {
+                f.imgtype = f.url.split('/0/')[1].split('.')[0].split('_')[0];
+              } else if (e.provider === 'planet') {
+                f.imgtype = f.url
+                  .split('_SR.tif/')[1]
+                  .split('.')[0]
+                  .split('_')[0];
+              } else {
+                f.imgtype = 'not supported';
+              }
+            });
+            e.date = e.date.substring(0, 10);
           });
-          e.date = e.date.substring(0, 10);
-        });
+        }
       });
   }
 
@@ -230,60 +276,103 @@ export class MapComponent implements OnInit, OnDestroy {
     });
   }
 
-  searchField() {
+  loadFieldImages() {
     this.isLoading = true;
-    this.appService
-      .loadImages(
-        this.getFieldId()!.value,
-        this.getStartDate()!.value,
-        this.getEndDate()!.value
-      )
-      .subscribe((data) => {
-        this.isLoading = false;
-        this.images = [];
-        this.images = data.images;
+    let fieldId = this.getFieldId()!.value;
+    let startDate = this.getStartDate()!.value;
+    let endDate = this.getEndDate()!.value;
+    if (fieldId === '' || startDate === '' || endDate === '') {
+      this.loadImagesMessage = 'You need to fill all the inputs.';
+      this.isLoading = false;
+    } else {
+      this.appService
+        .loadImages(fieldId, startDate, endDate)
+        .subscribe((data) => {
+          this.isLoading = false;
+          this.loadImagesMessage = '';
+          if (
+            data === 'No images were found for the time and field selected' ||
+            data === 'Request failed with status code 404'
+          ) {
+            this.loadImagesMessage = data;
+          } else {
+            this.images = [];
+            this.images = data.images;
 
-        var geometry = data.geometry[0][0];
+            var geometry = data.geometry[0][0];
 
-        var json = {
-          type: 'Feature' as GeoJsonTypes,
-          geometry: {
-            type: 'Polygon',
-            coordinates: [geometry],
-          },
-        };
+            var json = {
+              type: 'Feature' as GeoJsonTypes,
+              geometry: {
+                type: 'Polygon',
+                coordinates: [geometry],
+              },
+            };
 
-        var polygonStyle = {
-          weight: 2,
-          opacity: 1,
-          fill: false,
-          'fill-opacity': 0,
-        } as L.GeoJSONOptions;
+            var polygonStyle = {
+              weight: 2,
+              opacity: 1,
+              fill: false,
+              'fill-opacity': 0,
+            } as L.GeoJSONOptions;
 
-        this.bounds.clearLayers();
-        this.imagens.clearLayers();
+            this.bounds.clearLayers();
+            this.imagens.clearLayers();
 
-        var geoJsonLayer = L.geoJson(json, polygonStyle).addTo(this.bounds);
-        var geoBounds = geoJsonLayer.getBounds();
-        this.map.flyTo(geoJsonLayer.getBounds().getCenter(), 15);
+            var geoJsonLayer = L.geoJson(json, polygonStyle).addTo(this.bounds);
+            var geoBounds = geoJsonLayer.getBounds();
+            this.map.flyTo(geoJsonLayer.getBounds().getCenter(), 15);
 
-        this.images.forEach((e: any) => {
-          e.images.forEach((f: any) => {
-            f.bounds = geoBounds;
-            f.date = e.date.substring(11, 23);
-            if (e.provider === 'sentinel') {
-              f.imgtype = f.url.split('/0/')[1].split('.')[0].split('_')[0];
-            } else if (e.provider === 'planet') {
-              f.imgtype = f.url
-                .split('_SR.tif/')[1]
-                .split('.')[0]
-                .split('_')[0];
-            } else {
-              f.imgtype = 'not supported';
-            }
-          });
-          e.date = e.date.substring(0, 10);
+            this.images.forEach((e: any) => {
+              e.images.forEach((f: any) => {
+                f.bounds = geoBounds;
+                f.date = e.date.substring(11, 23);
+                if (e.provider === 'sentinel') {
+                  f.imgtype = f.url.split('/0/')[1].split('.')[0].split('_')[0];
+                } else if (e.provider === 'planet') {
+                  f.imgtype = f.url
+                    .split('_SR.tif/')[1]
+                    .split('.')[0]
+                    .split('_')[0];
+                } else {
+                  f.imgtype = 'not supported';
+                }
+              });
+              e.date = e.date.substring(0, 10);
+            });
+          }
         });
-      });
+    }
+  }
+
+  goFields() {
+    document.getElementById('create_field_a')!.click();
+  }
+
+  createField() {
+    this.isLoading = true;
+    let fieldId = this.getCreateFieldId().value;
+    let daysBefore = this.getCreateDaysBefore().value;
+    let bounderyType = this.getCreateBounderyType().value;
+    let fieldBoundery = this.getCreateFieldBoundery().value;
+    let provider = this.getCreateProvider().value;
+
+    if (
+      fieldId === '' ||
+      daysBefore === '' ||
+      bounderyType === '' ||
+      fieldBoundery === '' ||
+      provider === ''
+    ) {
+      this.isLoading = false;
+      this.createFieldMessage = 'You need to fill all the fields first.';
+    } else {
+      this.appService
+        .createField(fieldId, daysBefore, bounderyType, fieldBoundery, provider)
+        .subscribe((data) => {
+          this.isLoading = false;
+          this.createFieldMessage = data;
+        });
+    }
   }
 }
